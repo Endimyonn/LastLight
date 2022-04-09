ce0b2f4f33c6b2c36b8392432eeb29d5dbb32f0f
Endimyonn <joeakuaku@gmail.com>
2022-04-09 19:08:11 -0400

    remove URP, flashlight work

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashlight : MonoBehaviour
+{
+    public float powerLevel = 100;
+    public Light lightSource;
+    public float initialIntensity;
+
+    public float drainSpeed = 1;
+    public bool drainActive = true;
+
+    public float dimThreshold = 40;
+
+    public Slider powerMonitor;
+
+    
+    private void Awake()
+    {
+        initialIntensity = lightSource.intensity;
+    }
+
+    
+    private void Update()
+    {
+        //drain
+        if (drainActive)
+        {
+            if (powerLevel > 0)
+            {
+                powerLevel -= drainSpeed * Time.deltaTime;
+            }
+        }
+
+        //update light level
+        if (powerLevel <= dimThreshold)
+        {
+            lightSource.intensity = initialIntensity * (powerLevel / 100);
+        }
+        else if (lightSource.intensity != initialIntensity)
+        {
+            lightSource.intensity = initialIntensity;
+        }
+
+        //update UI
+        powerMonitor.value = powerLevel;
+    }
+
+    public void RestorePower(float argAmount)
+    {
+        powerLevel += argAmount;
+
+        if (powerLevel > 100)
+        {
+            powerLevel = 100;
+        }
+    }
+}
